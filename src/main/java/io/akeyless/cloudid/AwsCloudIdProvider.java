9a67775e64220f47bb07c5b0373e395ff4d3c5ff
Alexey <alikdolg@gmail.com>
2025-04-28 11:21:42 +0300

    Move AWS V1 -> V2

--- src/main/java/io/akeyless/cloudid/AwsCloudIdProvider.java
+++ src/main/java/io/akeyless/cloudid/AwsCloudIdProvider.java
@@ -1,15 +1,16 @@
 package io.akeyless.cloudid;
 
-import com.amazonaws.DefaultRequest;
-import com.amazonaws.auth.AWS4Signer;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.http.HttpMethodName;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.signer.Aws4Signer;
+import software.amazon.awssdk.auth.signer.params.Aws4SignerParams;
+import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.utils.StringInputStream;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHeaders;
 
-import java.io.ByteArrayInputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -19,39 +20,34 @@ import java.util.Map;
 public class AwsCloudIdProvider implements CloudIdProvider{
     @Override
     public String getCloudId() throws Exception {
-        AWSCredentialsProvider credentialsProvider = DefaultAWSCredentialsProviderChain.getInstance();
-        AWSCredentials credentials = credentialsProvider.getCredentials();
-
-        DefaultRequest<String> request = new DefaultRequest<>("sts");
+        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+        AwsCredentials credentials = credentialsProvider.resolveCredentials();
 
         String body = "Action=GetCallerIdentity&Version=2011-06-15";
-        request.setContent(new ByteArrayInputStream(body.getBytes()));
 
-        Map<String, String> headers = new LinkedHashMap<>();
-        headers.put(HttpHeaders.CONTENT_LENGTH, "" + body.length());
-        headers.put(HttpHeaders.CONTENT_TYPE,"application/x-www-form-urlencoded; charset=utf-8");
-        request.setHeaders(headers);
+        SdkHttpFullRequest unsignedRequest = SdkHttpFullRequest.builder()
+                .method(SdkHttpMethod.POST)
+                .uri(URI.create("https://sts.amazonaws.com"))
+                .encodedPath("/")
+                .putHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+                .putHeader("Content-Length", String.valueOf(body.length()))
+                .contentStreamProvider(() -> new StringInputStream(body))
+                .build();
 
-        request.setHttpMethod(HttpMethodName.POST);
-        request.setEndpoint(URI.create("https://sts.amazonaws.com"));
+        Aws4Signer signer = Aws4Signer.create();
 
-        AWS4Signer signer = new AWS4Signer();
-        signer.setServiceName("sts");
+        Aws4SignerParams signerParams = Aws4SignerParams.builder()
+                .signingName("sts")
+                .signingRegion(Region.US_EAST_1)
+                .awsCredentials(credentials)
+                .build();
 
-        signer.sign(request, credentials);
+        SdkHttpFullRequest signedRequest = signer.sign(unsignedRequest, signerParams);
 
-        Map<String, String> signedHeaders = request.getHeaders();
-        Map<String, String[]> signedHeadersArr = new LinkedHashMap<>();
-        for (Map.Entry<String, String> entry: signedHeaders.entrySet()
-             ) {
-            signedHeadersArr.put(entry.getKey(), new String[] {entry.getValue()});
-        }
-
-
-        String signedHeadersJson = new ObjectMapper().writeValueAsString(signedHeadersArr);
+        String signedHeadersJson = new ObjectMapper().writeValueAsString(signedRequest.headers());
 
         Map<String, String> awsData = new LinkedHashMap<>();
-        awsData.put("sts_request_method", String.valueOf(HttpMethodName.POST));
+        awsData.put("sts_request_method", SdkHttpMethod.POST.name());
         awsData.put("sts_request_url", Base64.getEncoder().encodeToString("https://sts.amazonaws.com/".getBytes(StandardCharsets.UTF_8)));
         awsData.put("sts_request_body", Base64.getEncoder().encodeToString(body.getBytes(StandardCharsets.UTF_8)));
         awsData.put("sts_request_headers", Base64.getEncoder().encodeToString(signedHeadersJson.getBytes(StandardCharsets.UTF_8)));
